19e9e4b5fc0c18235f743760917dcf32a6348d0e
milohill <116620023+milohill@users.noreply.github.com>
2023-05-31 11:52:32 +0900

    Create a login view

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
-  
-})
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  'is-member': { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('user', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 let express = require('express');
 
 let router = express.Router();
+const User = require('../models/user');
+
+const bcrypt = require('bcryptjs');
+const debug = require('debug')('time');
+
+const { body, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -8,12 +14,43 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/sign-up', (req, res, next) => {
+  const err = req.body;
   res.render('sign-up');
 });
 
-router.post('/sign-up', (req, res, next) => {
-  const result = req.body;
-  console.log(result);
+router.post('/sign-up', [
+  body('name').trim().escape(),
+  body('email').trim().escape(),
+  body('password').trim().escape(),
+
+  async (req, res, next) => {
+    // Error occurred in the form body
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render('sign-up', { err: errors });
+    }
+
+    // Passwords don't match
+    const { password } = req.body;
+    const confirmPassword = req.body['confirm-password'];
+    if (password !== confirmPassword) {
+      return res.render('sign-up', { err: `confirm password doesn't match` });
+    }
+
+    // If errors above don't occur, proceed to saving data
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.redirect('/login');
+  },
+]);
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
 });
 
 module.exports = router;
